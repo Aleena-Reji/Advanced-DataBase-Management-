70a2eb60e61848d33dce02d0fe0a646fc3c4974d
Aleena-Reji <aleenareji002@gmail.com>
2026-01-27 23:03:33 +0530

    Create Employee table and insert records
    Create Employee table and insert initial data.

---
+++ Pg2.sql
@@ -0,0 +1,28 @@
+//Progam 2-Execute DML statements 
+//Create table Employee with fields( Emp_ID, Emp_Name, DOB, Dept, Designation, Salary)
+
+CREATE TABLE Employee (
+    Emp_ID INT PRIMARY KEY,
+    Emp_Name VARCHAR(50),
+    DOB DATE,
+    Dept VARCHAR(30),
+    Designation VARCHAR(30),
+    Salary INT
+);
+
+//a)	Insert values into table
+
+INSERT INTO Employee 
+(Emp_ID, Emp_Name, DOB, Dept, Designation, Salary)
+VALUES
+(101, 'Rahul', '1998-05-15', 'IT', 'Software Engineer', 45000);
+
+INSERT INTO Employee 
+(Emp_ID, Emp_Name, DOB, Dept, Designation, Salary)
+VALUES
+(102, 'Anita', '1997-08-22', 'HR', 'HR Executive', 35000),
+(103, 'Vijay', '1996-11-10', 'Finance', 'Accountant', 40000);
+
+//b)	Write a basic SELECT query to retrieve all columns from a table.
+
+SELECT * FROM Employee;
